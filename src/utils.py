717ba6c45f9298dd115b9ab8706874101d4231d7
afgcc132 <69475033+Afgcc132@users.noreply.github.com>
2026-04-16 14:10:01 -0400

    Fix: pipeline de entrenamiento conectado y corrección de argumentos en ModelTrainer

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import dill
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -13,6 +14,29 @@ def save_object(file_path, obj):
         # Guardamos el objeto en modo 'wb' (write binary)
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)  # Entrenamos el modelo
+
+            # Hacemos predicciones
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Calculamos el R2 score para entrenamiento y prueba
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_score  # Guardamos solo el score de prueba
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
